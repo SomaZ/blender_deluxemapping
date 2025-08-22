fb73a91125fc8a9f36d7d536ce7410738d66a0f5
SomaZ <17459161+SomaZ@users.noreply.github.com>
2025-08-22 21:10:05 +0200

    [DLX] Attempt at fixing incorrect or missing denoises

--- source/blender/editors/object/object_bake_api.cc
+++ source/blender/editors/object/object_bake_api.cc
@@ -1811,13 +1811,16 @@ static int bake(const BakeAPIRender *bkr,
   }
 
   if (bkr->target != R_BAKE_TARGET_VERTEX_COLORS && bkr->use_bake_denoise_pp) {
-    size_t offset = 0;
     for (int j = 0; j < targets.images_num; j++) {
       const char* name = targets.images[j].image->id.name;
       if (!strcmp(name, "IM$vertmap_bake"))
         continue;
-      RE_denoise_bake(targets.images[j].width, targets.images[j].height, targets.channels_num, true, targets.result+offset);
-      offset += targets.images[j].width * targets.images[j].height * targets.channels_num;
+      RE_denoise_bake(
+        targets.images[j].width,
+        targets.images[j].height,
+        targets.channels_num,
+        true,
+        targets.result + targets.images[j].offset * targets.channels_num);
     }
   }
 
